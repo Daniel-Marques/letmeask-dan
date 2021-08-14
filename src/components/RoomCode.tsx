e51c227ad8d869a4b0b706293329cff64ffb4cca
Daniel Marques <daniel.silva.city@gmail.com>
2021-08-14 00:51:32 -0300

    Initial commit

---
+++ src/components/RoomCode.tsx
@@ -0,0 +1,30 @@
+import copyImg from "../assets/images/copy.svg";
+import toast, { Toaster } from "react-hot-toast";
+
+import "../styles/room-code.scss";
+
+type RoomCodeProps = {
+  code: string;
+};
+
+export function RoomCode(props: RoomCodeProps) {
+  function copyRoomCodeToClipboard() {
+    navigator.clipboard.writeText(props.code);
+    toast("Código copiado!", {
+      icon: "📋",
+    });
+  }
+
+  return (
+    <>
+      <Toaster />
+      <button className="room-code" onClick={copyRoomCodeToClipboard}>
+        <div>
+          <img src={copyImg} alt="Copy room code" />
+        </div>
+
+        <span>Sala #{props.code}</span>
+      </button>
+    </>
+  );
+}
---
+++ src/pages/NewRoom.tsx
@@ -0,0 +1,65 @@
+import { FormEvent, useState } from "react";
+import { Link, useHistory } from "react-router-dom";
+
+import illustrationImg from "../assets/images/illustration.svg";
+import logoImg from "../assets/images/logo.svg";
+
+import { Button } from "../components/Button";
+import { database } from "../services/firebase";
+
+import "../styles/auth.scss";
+import { useAuth } from "../hooks/useAuth";
+
+export function NewRoom() {
+  const { user } = useAuth();
+  const [newRoom, setNewRoom] = useState("");
+  const history = useHistory();
+
+  async function handleCreateRoom(event: FormEvent) {
+    event.preventDefault();
+
+    if (newRoom.trim() === "") {
+      return;
+    }
+
+    const roomRef = database.ref("rooms");
+
+    const firebaseRoom = await roomRef.push({
+      title: newRoom,
+      authorId: user?.id,
+    });
+
+    history.push(`/rooms/${firebaseRoom.key}`);
+  }
+
+  return (
+    <div id="page-auth">
+      <aside>
+        <img
+          src={illustrationImg}
+          alt="Ilustração simbolizando perguntas e respostas"
+        />
+        <strong>Crie uma nova sala</strong>
+        <p>Tire as dúvidas da sua audiência em tempo-real</p>
+      </aside>
+
+      <main>
+        <div className="main-content">
+          <img src={logoImg} alt="Letmeask" />
+          <h2>Criar uma nova sala</h2>
+          <form onSubmit={handleCreateRoom}>
+            <input
+              type="text"
+              placeholder="Digite o código da sala"
+              onChange={(event) => setNewRoom(event.target.value)}
+            />
+            <Button type="submit">Entrar na sala</Button>
+          </form>
+          <p>
+            Quer entrar em uma sala existente? <Link to="/">Clique aqui</Link>
+          </p>
+        </div>
+      </main>
+    </div>
+  );
+}
